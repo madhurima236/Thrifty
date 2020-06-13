0d3dfc96704183fc8835e301bab967a0db7686fd
madhurima <madhurima.dutta@mail.utoronto.ca>
2020-06-13 16:06:39 +0530

    Added the tesseract and now you can test it. I have added some pictures to look at the accuracy.

---
+++ MainReader/OCR.py
@@ -0,0 +1,18 @@
+import cv2
+import pytesseract
+
+
+def read_image(file) -> str:
+    img = cv2.imread(file)
+
+    # Adding custom options
+    custom_config = r'--oem 3 --psm 6'
+    img_text = pytesseract.image_to_string(img, config=custom_config)
+    return img_text
+
+
+if __name__ == '__main__':
+    text = read_image(
+        '/Users/madhurima/PycharmProjects/ReceiptManagement/MainReader'
+        '/Pictures to test/TheCurator-3140.jpg')
+    print(text)
