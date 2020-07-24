ce2d8a2e3432aeb70b01d694b4a26b1577f72891
madhurima <madhurima.dutta@mail.utoronto.ca>
2020-07-24 20:20:01 +0530

    Added more receipts.

--- MainReader/classifytext.py
+++ MainReader/classifytext.py
@@ -11,7 +11,6 @@ from google.cloud.language import types
 from google.oauth2 import service_account
 import os
 import json
-from google.cloud import vision
 import io
 
 import cv2
@@ -19,33 +18,6 @@ import pytesseract
 from PIL import Image
 from datetime import date
 
-
-def detect_text(path):
-    """Detects text in the file."""
-
-    cred_dict = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-    credentials = service_account.Credentials.from_service_account_info(
-        cred_dict)
-    # Instantiates a client
-    client = vision.ImageAnnotatorClient(credentials=credentials)
-
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
-
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
 
 def classify(text) -> str:
     """Classify the input text into categories. """
@@ -100,29 +72,37 @@ class Receipt:
     items_to_price = {}
     categories_to_price = {}
     categories_to_items = {}
+    total = 0.0
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.date = date.today()
 
     def get_data(self) -> None:
-        im = Image.open(self.filepath)
         image = cv2.imread(self.filepath, cv2.IMREAD_GRAYSCALE)
-        _, image_enhanced = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
-        # filter = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        # sharpen_img = cv2.filter2D(image_enhanced, -1, filter)
-        # image_enhanced = cv2.equalizeHist(image)
+        _, image_enhanced = cv2.threshold(image, 180, 255, cv2.THRESH_BINARY)
         image_enhanced = cv2.bilateralFilter(image_enhanced, 9, 75, 75)
-        cv2.imwrite("/Users/madhurima/PycharmProjects/ReceiptManagement/MainReader/WalmartReceipts/enhanced_image.png", image_enhanced)
+        # cv2.imwrite("/Users/madhurima/PycharmProjects/ReceiptManagement/MainReader/WalmartReceipts/enhanced_image.png", image_enhanced)
         text = pytesseract.image_to_string(image_enhanced, lang='eng')
 
         print(text)
+        ignore_items_lst = ['subtotal', 'total', 'tax', 'change', 'tips',
+                            'change due', 'tant', 'sub total', 'hst']
+        to_ignore = False
 
-        pattern_line = re.compile(r'([A-Za-z ]+).* (\d+\.\d+)')
+        pattern_line = re.compile(r'([A-Za-z][A-Za-z ]*).* (\d+\.\d+)')
 
         items = {}
         for matches_line in pattern_line.findall(text):
-            items[matches_line[0]] = float(matches_line[1])
+            if matches_line[0].strip().lower() not in ignore_items_lst and not to_ignore:
+                if matches_line[0].strip() not in items:
+                    items[matches_line[0].strip()] = float(matches_line[1])
+                else:
+                    items[matches_line[0].strip()] += float(matches_line[1])
+            else:
+                to_ignore = True
+                if matches_line[0].strip().lower() == 'total':
+                    self.total += float(matches_line[1])
 
         self.items_to_price = items
 
@@ -169,7 +149,8 @@ class Receipt:
 
 
 if __name__ == '__main__':
-    receipt = Receipt('/Users/madhurima/PycharmProjects/ReceiptManagement/MainReader/WalmartReceipts/9C5Q9Rt.jpg')
+    receipt = Receipt('/Users/madhurima/PycharmProjects/ReceiptManagement/MainReader/WalmartReceipts/receipt-ocr-original.jpg.png')
     receipt.get_data()
     print(receipt.items_to_price)
+    print(receipt.total)
     # detect_text("
