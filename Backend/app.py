c19a6262709c03479f65c8aca9257c3e0b7f6c5d
Advaya24 <advaya24@yahoo.co.in>
2020-07-31 17:15:22 +0530

    Completed categorize in app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,19 +1,27 @@
-from flask import Flask, redirect, render_template, request, send_file, json
+from flask import Flask, request, send_file, json
 from flask_cors import CORS
+from Backend.statistics import *
 
 
 app = Flask(__name__)
 CORS(app)
 
+receipts = MultipleReceipts()
+
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!', 200
 
+
 @app.route('/categorize', methods=['POST'])
 def categorize():
     file = request.files['receipt']
+    file.save('Backend/WalmartReceipts/receipt')
+    receipts.add_receipt('Backend/WalmartReceipts/receipt')
+    return receipts.categories_to_prices
 
 
-# 1. Category to items
+
+# 1. Category to items - Done!
 # 2. Pie Chart
 # 3. Bar Graph
