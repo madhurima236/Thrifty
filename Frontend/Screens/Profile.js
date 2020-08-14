60adcbf24e96bf5be0b8f47b0509e708b4807ab5
madhurima <madhurima.dutta@mail.utoronto.ca>
2020-08-14 13:27:53 +0530

    Updated profile

--- Frontend/Screens/Profile.js
+++ Frontend/Screens/Profile.js
@@ -20,76 +20,60 @@ import {
 } from "native-base";
 import firebase from "../Database/firebase";
 import PhotoGrid from 'react-native-photo-grid';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
 // import Settings from "./Settings";
+
+var images = ['/Users/madhurima/Desktop/Thrifty/Frontend/assets/DemoPics/Unknown-2.png', 
+'/Users/madhurima/Desktop/Thrifty/Frontend/assets/DemoPics/Unknown-3.png', 
+'/Users/madhurima/Desktop/Thrifty/Frontend/assets/DemoPics/Unknown-4.png']
+
+var {width, height} = Dimensions.get('window')
 
 class Profile extends Component {
   constructor(props) {
     super(props)
-    this.state = { items: [] };
     firebase.database().ref(`${firebase.auth().currentUser.uid}`).once('value')
       .then((snapshot) => {
         console.log(snapshot.val());
       });
   }
 
-  componentDidMount() {
-    // Build an array of 60 photos
-    let items = Array.apply(null, Array(60)).map((v, i) => {
-      return { id: i, src: 'http://placehold.it/200x200?text='+(i+1) }
+  rendersection() {
+    return images.map((image, index) => {
+      return(
+        <View key={index} style={[{width:(width)/3}, {height:(width)/3}]}>
+          <Image 
+          style={{flex:1, width:undefined, height:undefined }}
+          source={{uri="https://firebasestorage.googleapis.com/v0/b/thrifty-c8d4b.appspot.com/o/receipt_10?alt=media&token=09e78494-062e-4e3e-a9e0-ed027126ff4c"}}
+          />
+        </View>
+      )
     });
-    this.setState({ items });
   }
-
 
   render() {
     return (
       <View style={styles.container}>
-        <Text> Profile </Text>
+        {/* <Text> Profile </Text>
         <Button
           color="#00d800"
           title="Settings"
           onPress={() => navigation.navigate("Settings")}
-        />
-        {/* <Button
-          color="#00d800"
-          title="Logout"
-          style={{ width: 80, height: 80 }}
-          onPress={() => this.signOut()} */}
-        {/* /> */}
-        <PhotoGrid
-        data = { this.state.items }
-        itemsPerRow = { 3 }
-        itemMargin = { 1 }
-        renderHeader = { this.renderHeader }
-        renderItem = { this.renderItem }
-      />
-        {/* */}
+        /> */}
+        <Content> 
+          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+            {this.rendersection()}
+          </View>
+        </Content>
       </View>
     );
   }
-
-  renderHeader() {
-    return(
-      <Text>I'm on top!</Text>
-    );
-  }
-
-  renderItem(item, itemSize) {
-    return(
-      <TouchableOpacity
-        key = { item.id }
-        style = {{ width: itemSize, height: itemSize }}
-        onPress = { () => {
-          console.log(1)
-        }}>
-        <Image
-          resizeMode = "cover"
-          style = {{ flex: 1 }}
-          source = {{ uri: item.src }}
-        />
-      </TouchableOpacity>
-    )
-  }
+  
 }
 export default Profile;
 
